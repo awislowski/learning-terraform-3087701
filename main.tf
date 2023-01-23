baa727e567e308ca74b02b6ff98173f82482432d
Andrzej Wisłowski <awislowski@gmail.com>
2023-01-23 12:21:34 +0100

    autoscaling

--- main.tf
+++ main.tf
@@ -14,16 +14,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.7.0"
+ 
+ 
+  name = "blog"
+  min_size = 1
+  max_size = 2
+  vpc_zone_indentifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
 
-  subnet_id              = module.blog_vpc.public_subnets[0]
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  image_id           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type  
 
-  tags = {
-    Name = "HelloWorld"
-  }
 }
 
 module "blog_sg" {
@@ -95,3 +100,4 @@ module "blog_alb" {
     Environment = "dev"
   }
 }
+
